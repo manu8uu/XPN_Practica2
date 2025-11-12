69c971ad808e6b8f7730374c72e92a342963f895
pablodiazo <pablo.diazo@udc.es>
2025-11-12 19:28:36 +0100

    Añadidos archivos iniciales

---
+++ xpn/src/test/java/com/example/xpn/XpnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xpn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
